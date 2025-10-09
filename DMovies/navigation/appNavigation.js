045596486c57ed1978294972e9a7255ef85be269
DanielVianaC <daniel.carvalho85@etec.sp.gov.br>
2025-10-09 12:22:28 -0300

    Programação DMovie

---
+++ DMovies/navigation/appNavigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigation } from 'react-navigation/native-stack'
+import HomeScreen from '../screens/HomeScreen';
+import { StackScreen } from 'react-native-screens';
+
+export default function appNavigation() {
+  return (
+    <NavigationContainer>
+        <StackActions.Navigator>
+            <StackScreen
+                name = "Home"
+                options={{ headerShown: false }}
+                component={{HomeScreen}}
+            />
+        </StackActions.Navigator>
+    </NavigationContainer>
+  )
+}
