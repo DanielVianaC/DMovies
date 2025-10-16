3bae952d76e05fcfb32cdf597f7c5005c3dba8a3
DanielVianaC <daniel.carvalho85@etec.sp.gov.br>
2025-10-16 12:25:09 -0300

    Reecriação do Projeto

--- DMovies/screens/HomeScreen.js
+++ DMovies/screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function HomeScreens() {
+export default function HomeScreen() {
   return (
     <View>
-      <Text>HomeScreens</Text>
+      <Text>HomeScreen</Text>
     </View>
   )
 }
